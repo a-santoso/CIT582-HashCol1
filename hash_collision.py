0058fb3134329722fa81b748106818c78d6f8258
a-santoso <49494246+a-santoso@users.noreply.github.com>
2022-01-16 16:30:38 +0700

    Update hash_collision.py

--- hash_collision.py
+++ hash_collision.py
@@ -3,7 +3,9 @@ import os
 
 lookup_table = {}
 
+
 def hash_collision(k):
+
     if not isinstance(k, int):
         print("hash_collision expects an integer")
         return (b'\x00', b'\x00')
@@ -12,25 +14,24 @@ def hash_collision(k):
         return (b'\x00', b'\x00')
 
     # Collision finding code goes here
-    for value in range(10):
+    for i in range(100000):
 
         random_binary = os.urandom(8)
         result = hashlib.sha256(random_binary).digest()
         result = result[:k]
         if result in lookup_table:
-            print("Collission found")
-            print("String 1: " + random_binary, "Matching bits: " + result[:k])
-            print("String 2: " + lookup_table[result], "Matching bits: " + result[:k])
-            x = random_binary
-            y = lookup_table[result]
+            print("Collision found")
+            print(random_binary, result)
+            print(lookup_table[result], result)
+
         else:
             lookup_table[result] = random_binary
 
-#    x = b'\x00'
-#    y = b'\x00'
+    x = random_binary
+    y = lookup_table[result]
 
     return (x, y)
 
 
-if __name__ == '__hash_collision__':
-    hash_collision(500)
+if __name__ == '__main__':
+    hash_collision(3)
